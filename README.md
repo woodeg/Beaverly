1bb6015bb7eaf140a275d44991ea2e4f5a176447
Georg Schurkin <georg.schurkin@medienhandwerk.info>
2021-09-27 12:55:16 +0200

    v0.0.2. Add User

--- README.md
+++ README.md
@@ -18,14 +18,20 @@ Framework: Symfony
 
 ### Start
 
-1. New project: *composer create-project symfony/website-skeleton Beaverly*
+#### 27.09.2021
 
-2. GIT:
-git init
-git add --all
-add to .gitignore .env config files
-git commit -m"1. Start"
-Install and sign in "GitHub Pull Request and Issues Extention"
-git remote add origin [Beaverly on GIT](https://github.com/woodeg/Beaverly.git)
-git pull
-git push origin master
+* New project: *composer create-project symfony/website-skeleton Beaverly*
+* GIT:
+
+>git init
+>git add --all
+>add to .gitignore .env config files
+>git commit -m"1. Start"
+>Install and sign in "GitHub Pull Request and Issues Extention"
+>git remote add origin [Beaverly on GIT](https://github.com/woodeg/Beaverly.git)
+>git pull
+>git push origin master
+
+* Add User entity: *symfony console make:user*
+* Add attributs **firstName, lastName, isActive, userDescription** to User entity: *symfony console make:entity User*
+* Add to GIT
---
+++ src/Entity/User.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $userDescription;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getUserDescription(): ?string
+    {
+        return $this->userDescription;
+    }
+
+    public function setUserDescription(?string $userDescription): self
+    {
+        $this->userDescription = $userDescription;
+
+        return $this;
+    }
+}
